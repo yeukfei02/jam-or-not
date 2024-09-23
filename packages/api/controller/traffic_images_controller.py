2b3281d86a3434a8b1e290ba2d807dc0c1df0b93
Donald Wu <yeukfei02@gmail.com>
2024-09-23 11:36:05 +0800

    fix: add services folder

--- packages/api/controller/traffic_images_controller.py
+++ packages/api/controller/traffic_images_controller.py
@@ -1,7 +1,6 @@
-import os
-import requests
 from fastapi import Request, status
 from fastapi.responses import JSONResponse
+from services.traffic_images_service import get_images_api
 
 
 def traffic_images_controller(request: Request):
@@ -19,82 +18,3 @@ def traffic_images_controller(request: Request):
 
     response = JSONResponse(status_code=status.HTTP_200_OK, content=data)
     return response
-
-
-def get_images_api():
-    result = {}
-
-    try:
-        root_url = "https://datamall2.mytransport.sg/ltaodataservice/Traffic-Imagesv2"
-
-        account_key = os.getenv("ACCOUNT_KEY")
-
-        headers = {
-            "AccountKey": account_key
-        }
-
-        response = requests.get(root_url, headers=headers)
-        print(f"response = {response}")
-
-        if response.status_code == 200:
-            response_json = response.json()
-            # print(f"response_json = {response_json}")
-
-            if response_json:
-                values = response_json.get("value")
-
-                if values:
-                    for item in values:
-                        camera_id = item.get("CameraID")
-                        latitude = item.get("Latitude")
-                        longitude = item.get("Longitude")
-                        image_link = item.get("ImageLink")
-
-                        if camera_id == "2701":
-                            data = {
-                                "camera_id": camera_id,
-                                "latitude": latitude,
-                                "longitude": longitude,
-                                "image_link": image_link
-                            }
-                            result["woodland_johor_bridge"] = data
-
-                        if camera_id == "4703":
-                            data = {
-                                "camera_id": camera_id,
-                                "latitude": latitude,
-                                "longitude": longitude,
-                                "image_link": image_link
-                            }
-                            result["tuas_second_link"] = data
-
-                        if camera_id == "2702":
-                            data = {
-                                "camera_id": camera_id,
-                                "latitude": latitude,
-                                "longitude": longitude,
-                                "image_link": image_link
-                            }
-                            result["woodland_checkpoint"] = data
-
-                        if camera_id == "2704":
-                            data = {
-                                "camera_id": camera_id,
-                                "latitude": latitude,
-                                "longitude": longitude,
-                                "image_link": image_link
-                            }
-                            result["towards_woodland_checkpoint"] = data
-
-                        if camera_id == "4713":
-                            data = {
-                                "camera_id": camera_id,
-                                "latitude": latitude,
-                                "longitude": longitude,
-                                "image_link": image_link
-                            }
-                            result["tuas_checkpoint"] = data
-    except Exception as e:
-        print(f"get_images_api error = {e}")
-
-    return result
